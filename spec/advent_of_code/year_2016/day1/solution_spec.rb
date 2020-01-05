33178c66953eb4dbcb4b6e7da41a8d269250dc7c
kirill.shved <kirill.shved@cleverlabs.io>
2020-01-05 18:09:10 +0300

    Refactor test of year2016/day1

--- spec/advent_of_code/year_2016/day1/solution_spec.rb
+++ spec/advent_of_code/year_2016/day1/solution_spec.rb
@@ -2,14 +2,12 @@
 
 require 'spec_helper'
 
-RSpec.describe AdventOfCode::Year2016::Day1::Solution do
+describe AdventOfCode::Year2016::Day1::Solution do
   describe '#call' do
     subject { described_class.new }
 
     let(:step_count) { 209 }
 
-    it 'return correct count of steps' do
-      expect(subject.call).to eq(step_count)
-    end
+    it { expect(subject.call).to eq(step_count) }
   end
 end
